27cb8ef93ecc4cb3cab2943f64b8fe9f67e935e1
alejandro_Chapina <alejandrochapina92@gmail.com>
2023-02-10 17:27:55 -0600

    Practica Control

---
+++ JS/js03/js/ejercicio.js
@@ -0,0 +1,32 @@
+let diaNumero = parseInt(prompt("Ingrese un número entre 0-6 para conocer el día de la semana: " ))
+
+
+
+if (diaNumero ===0){
+    document.write("El día es Domingo")
+} else if (diaNumero ===1){
+    document.write("El día es Lunes")
+} else if (diaNumero ===2){
+    document.write("El día es Martes")
+} else if (diaNumero ===3){
+    document.write("El día es Miercoles")
+} else if (diaNumero ===4){
+    document.write("El día es Jueves")
+} else if (diaNumero ===5){
+    document.write("El día es Viernes")
+} else if (diaNumero ===6){
+    document.write("El día es Sabado")
+} else {
+    document.write("El número ingresado es incorrecto")
+}
+
+let q = true;
+let p = true;
+
+if (q && p) console.log()
+
+//Operadores logicos
+
+//and &&
+// or ||
+// not !

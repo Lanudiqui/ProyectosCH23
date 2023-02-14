a7324143235f9142d26bc1e04e3dc64693c0ff1d
alejandro_Chapina <alejandrochapina92@gmail.com>
2023-02-14 00:06:18 -0600

    Arrays and loops

---
+++ JS/js04/js/index.js
@@ -0,0 +1,73 @@
+// let miVariable = 5;
+// let miArray = [2, 3, 4, 5, 6, 7];
+// let myarray = new Array();
+
+// console.log(miArray[0]);
+// console.log(miArray[1]);
+
+// let myArray = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+
+// console.log("Mi array " + myArray[0][1]);
+// console.log("Mi array " + myArray[0][2]);
+// console.log("Mi array " + myArray[2][1]);
+// console.log("Mi array " + myArray[1][2]);
+// console.log("Todo el Indice " + myArray[0]);
+// console.log("Todo el Indice " + myArray[1]);
+// console.log("Todo el Indice " + myArray[2]);
+
+/***********Ciclo For***********/
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("Imprimiendo i ->" + i);
+
+// }
+
+// for (let i = 0; i <= 5; i++) {
+//     console.log("Imprimiendo miArray ->" + miArray[i]);
+
+// }
+
+/************Recorrer un arreglo multidimensional***********/
+
+// for (let i = 0; i < 3; i++) {
+
+//     for (let j = 0; j < 3; j++) {
+//         console.log("Imprimiendo arreglo multidimensional -> " + myArray[i][j]);
+
+//     }
+//     console.log("***********************")
+
+// }
+
+/***********Multiplicación de matrices********** */
+// let secondArray = [[],[],[]]
+
+// for (let i = 0; i < 3; i++) {
+
+//     for (let j = 0; j < 3; j++) {
+//         secondArray[i][j] = myArray[i][j] * myArray[i][j];
+
+//     }
+// }
+
+// console.log("Imprimiendo multiplicación de dos matrices -> " + secondArray);
+
+/***********Ciclo While**********/
+/*
+let variable inicio;
+
+while(Condicional){
+
+ } 
+ */
+
+ let contador = 0;
+ while(contador<=10){
+    console.log(contador);
+    contador++;
+
+ }
